b70b1d566ed68a1f6533034b520c88e1260c6d70
gokhancag <gokhancag@gokhancag.com>
2024-09-03 11:19:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Merhaba Garanti!"
+    return "Merhaba Garanti2!"
 
 @app.route('/nasilsin')
 def hello():
-    return 'iyiyim, sen nasilsin Garanti?'
+    return 'iyiyim, sen nasilsin Garanti2?'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
